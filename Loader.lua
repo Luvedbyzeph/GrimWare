345ea266f482fc7d0625d32c7d74a5dae55a0c6c
Zeph <114033331+Luvedbyzeph@users.noreply.github.com>
2025-09-21 20:11:07 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/5fa03b0cd8e24c9cbd650baf12a9ae56"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/3cc49ab264ad44f2827272bc3061da73"))()
